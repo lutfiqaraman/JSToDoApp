22cf009dce788e5027a39df76a0dd43d58fa3fc3
lutfiqaraman <lutfiqaraman@gmail.com>
2022-05-30 20:38:05 +0300

    build todo app

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
-let form = document.getElementById("form");
+let form  = document.getElementById("form");
+let input = document.getElementById("input");
+let msg   = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    formValidation();
+});
+
+let formValidation = () => {
+  if (input.value === "") {
+      msg.innerHTML = "You should write a post";
+  } else {
+      msg.innerHTML = "";
+  }
+};
+
 
